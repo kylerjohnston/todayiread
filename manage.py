dfdd0bb1f0d51898b8110bd07f17a55769670d63
kylerjohnston <kjhnstn2@illinois.edu>
2016-02-21 09:28:23 -0600

    Initial db migration set up with Flask-Migrate

--- manage.py
+++ manage.py
@@ -3,10 +3,12 @@
 from kylereads import create_app, db
 from kylereads.models import User, ReadingSession, Title
 from flask.ext.script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
 import os
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 def make_shell_context():
     return dict(app = app,
@@ -17,6 +19,8 @@ def make_shell_context():
 
 manager.add_command('shell',
                     Shell(make_context = make_shell_context))
+
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
