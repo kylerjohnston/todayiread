1cbff81f4726c439a2b5f82a325857aa317a6760
kylerjohnston <kjhnstn2@illinois.edu>
2016-02-28 13:45:52 -0600

    Fixes issue with user_id foreign key being string not int

--- kylereads/models.py
+++ kylereads/models.py
@@ -68,7 +68,7 @@ class User(UserMixin, db.Model):
 class ReadingSession(db.Model):
     __tablename__ = 'readingsession'
     id = db.Column(db.Integer, primary_key = True)
-    user_id = db.Column(db.String(80), db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     user = db.relationship('User', backref = db.backref('sessions', lazy = 'dynamic'))
     title_id = db.Column(db.Integer, db.ForeignKey('title.id'))
     title = db.relationship('Title', backref = db.backref('readingsessions', lazy =  'dynamic'))
