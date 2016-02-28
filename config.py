81def7beeeb89b9119b59561fac0d8719d1a8ecd
kylerjohnston <kjhnstn2@illinois.edu>
2016-02-28 13:17:52 -0600

    Adds wsgi.py and updates production db

--- config.py
+++ config.py
@@ -7,8 +7,10 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    DB_USERNAME = os.environ.get('READ_DB_USERNAME')
+    DB_PASSWORD = os.environ.get('READ_DB_PASSWORD')
     MAIL_SUBJECT_PREFIX = '[Today I Read] '
-    MAIL_DEFAULT_SENDER = ('Admin', 'kylerjohnston@gmail.com')
+    MAIL_DEFAULT_SENDER = ('Admin', MAIL_USERNAME)
 
     @staticmethod
     def init_app(app):
@@ -25,8 +27,9 @@ class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = ''
 
 class ProductionConfig(Config):
+    DB_NAME = 'todayiread'
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = ''
+    SQLALCHEMY_DATABASE_URI = 'postgresql://' + Config.DB_USERNAME + ':' + Config.DB_PASSWORD + 'localhost/' + DB_NAME
 
 config = {
     'dev': DevelopmentConfig,
